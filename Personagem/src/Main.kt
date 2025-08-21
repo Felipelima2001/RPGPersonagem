aaccc3347f9c65d49fb925aec6eb8ac637ef096b
LYUPX <felipelima282001@gmail.com>
2025-08-21 19:13:44 -0300

    Adicionando metodo de inserir dados do personagem pelo terminal.

--- Personagem/src/Main.kt
+++ Personagem/src/Main.kt
@@ -1,18 +1,58 @@
-// Main.kt
+
 import personagem.Personagem
 import model.TiposDistribuicao
-import model.classes.*
-import model.racas.*
+import model.classes.Ladino
+import model.classes.Mago
+import model.classes.Guerreiro
+import model.racas.Anao
+import model.racas.Elfo
+import model.racas.Humano
 import utils.DistribuicaoAtributos
 
 fun main() {
-    val atributos = DistribuicaoAtributos.distribuir(TiposDistribuicao.Classica)
-    val personagem = Personagem("Aragorn", atributos, Humano(), Guerreiro())
-    personagem.mostrarFicha()
+        println("=== CRIAÇÃO DE PERSONAGEM ===")
 
-    val personagem2 = Personagem("Gandalf", DistribuicaoAtributos.distribuir(TiposDistribuicao.Herioca), Elfo(), Mago())
-    personagem2.mostrarFicha()
 
-    val personagem3 = Personagem("Bilbo", DistribuicaoAtributos.distribuir(TiposDistribuicao.Aventureira), Anao(), Ladino())
-    personagem3.mostrarFicha()
-}
+        print("Digite o nome do personagem: ")
+        val nome = readln()
+
+
+        println("Escolha a distribuição de atributos:")
+        println("1 - Clássica")
+        println("2 - Heróica")
+        println("3 - Aventureiro")
+        val escolhaDist = readln().toInt()
+        val tipoDistribuicao = when (escolhaDist) {
+            1 -> TiposDistribuicao.Classica
+            2 -> TiposDistribuicao.Herioca
+            else -> TiposDistribuicao.Aventureira
+        }
+        val atributos = DistribuicaoAtributos.distribuir(tipoDistribuicao)
+
+
+        println("Escolha a raça:")
+        println("1 - Humano")
+        println("2 - Elfo")
+        println("3 - Anão")
+        val escolhaRaca = readln().toInt()
+        val raca = when (escolhaRaca) {
+            1 -> Humano()
+            2 -> Elfo()
+            else -> Anao()
+        }
+
+        println("Escolha a classe:")
+        println("1 - Guerreiro")
+        println("2 - Mago")
+        println("3 - Ladino")
+        val escolhaClasse = readln().toInt()
+        val classe = when (escolhaClasse) {
+            1 -> Guerreiro()
+            2 -> Mago()
+            else -> Ladino()
+        }
+
+        val personagem = Personagem(nome,atributos,raca,classe)
+
+        personagem.mostrarFicha()
+    }
